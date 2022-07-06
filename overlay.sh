70a93c66d55d59c74a505e816059b32585182f91
Khanh Nguyen <khanh93vn@gmail.com>
2022-07-06 13:56:31 +0700

    fix brackets bug in bash scripts

--- overlay.sh
+++ overlay.sh
@@ -1,6 +1,6 @@
 # source custom packages
-(
+{
   echo -n "Setting up overlay packages... " &&
   source $ROS_WORKSPACE/install/local_setup.bash &&
   echo "Done"
-)
+}
--- underlay.sh
+++ underlay.sh
@@ -1,31 +1,31 @@
 # source ros2 setup file
-(
-  (
-    (
+{
+  {
+    {
       echo -n "Setting up humble version... " &&
       source /opt/ros/humble/setup.bash
-    ) ||
-    (
+    } ||
+    {
       echo -n -e "\nSetting up foxy version instead... " &&
       source /opt/ros/foxy/setup.bash
-    )
-  ) &&
+    }
+  } &&
   echo "Done"
-)
+}
 
 # enable colcon argcomplete
-(
+{
   echo -n "Eabling colcon auto-complete... " &&
   source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash &&
   source /usr/share/colcon_cd/function/colcon_cd.sh &&
   export _colcon_cd_root=/opt/ros/$ROS_DISTRO/ &&
   echo "Done"
-)
+}
 
 # export environmental variables
-(
+{
   echo -n "Exporting environmental variables... " &&
   export ROS_DOMAIN_ID=4 &&
   export LC_NUMERIC="en_US.UTF-8" &&
   echo "Done"
-)
+}
