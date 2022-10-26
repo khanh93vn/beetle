9893a34bf83f2b7e28295844949f745b82665a55
Khanh Nguyen <khanh93vn@gmail.com>
2022-10-26 11:37:55 +0700

    finish beetle_evaluation package

--- src/beetle_evaluation/beetle_evaluation/beetle_evaluation_manager.py
+++ src/beetle_evaluation/beetle_evaluation/beetle_evaluation_manager.py
@@ -1,5 +1,8 @@
+import os
+
 from math import pi, sin, cos, hypot, degrees
 from itertools import product
+from time import sleep
 
 import rclpy
 
@@ -8,15 +11,16 @@ from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg._quaternion import Quaternion
 from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
 from tf_transformations import quaternion_about_axis, euler_from_quaternion
-from gazebo_msgs.srv import GetEntityState
+from gazebo_msgs.srv import GetEntityState, SetEntityState
 
 
 # Constants --------------------------------------------------------------------
-DEBUG = True
+data_file_path = os.path.join(
+    os.environ["HOME"], "Documents/data/beetle_evaluation_data.csv")
 
 # Experiment samples
-sample_distances = [2.0, 6.0]
-sample_directions = [i*(pi/3) for i in range(4)]
+sample_distances = [6.0, 12.0]
+sample_directions = [-i*(pi/3) for i in range(4)]
 sample_headings = [i*(pi/4) for i in range(8)]
 timer_period = 1.0  # seconds
 
@@ -24,86 +28,77 @@ timer_period = 1.0  # seconds
 # Class BeetleEvaluationManager
 
 class BeetleEvaluationManager(Node):
+    robot_name = 'beetle'
+    marker_name = 'arrow'
+    camera_name = 'user_camera'
+    camera_orientation = Quaternion(
+        x=0.0, y=0.7071067811865475, z=0.0, w=0.7071067811865476)
 
     def __init__(self):
         super().__init__('beetle_evaluation_manager')
 
-        # Initialize gazebo clients
-        self.get_entity_state_client = \
-            self.create_client(GetEntityState, '/get_entity_state')
-        self.get_entity_state_req = GetEntityState.Request()
-        self.get_entity_state_req.name = 'beetle'
-        self.get_entity_state_req.reference_frame = ''
-        self.get_entity_state_res = GetEntityState.Response()
-        if self.get_entity_state_client.wait_for_service(timeout_sec=3.0):
-            if self.get_entity_state_res.success:
-                self.current_position = self.get_entity_state_res.state.pose
-            self.get_entity_state_call = \
-                self.get_entity_state_client.call_async(self.get_entity_state_req)
-            self.get_logger().info("Đợi lấy dữ liệu vị trí xe")
-            while not self.get_entity_state_call.done():
-                rclpy.spin_once(self)
-            self.get_entity_state_res = self.get_entity_state_call.result()
-            if self.get_entity_state_res.success:
-                self.current_pose = self.get_entity_state_res.state.pose
-        else:
-            raise RuntimeError("Không thể lấy dữ liệu vị trí từ Gazebo")
+        # Initialize clients
+        self._initialize_node()
 
-
+        # Initialize navigator
         self._initialize_navigator()
 
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        # Reset experiments
+        self.reset_experiments()
 
-    def timer_callback(self):
-        if self.get_entity_state_call.done():
-            self.get_entity_state_res = \
-                self.get_entity_state_call.result()
-            if self.get_entity_state_res.success:
-                self.current_pose = self.get_entity_state_res.state.pose
-        self.get_entity_state_call = \
-            self.get_entity_state_client.call_async(self.get_entity_state_req)
-
-        if self.current_experiment_index >= len(self.relative_goals):
-            return
-
-        if self.navigator.isTaskComplete():
-            if self.current_experiment_index > 0:
-                self.collect_results()
-            self.begin_experiment(self.current_experiment_index)
-            self.current_experiment_index += 1
-
+    def _initialize_node(self):
+        self.get_client = \
+            self.create_client(GetEntityState, '/get_entity_state')
+        self.set_client = \
+            self.create_client(SetEntityState, '/set_entity_state')
 
     def _initialize_navigator(self):
         self.navigator = BasicNavigator()
 
         # Set initial pose
-        self.get_logger().info("Đang giúp xe set trạng thái đầu")
+        self.get_logger().info("Đang giúp xe set trạng thái đầu...")
         initial_pose = PoseStamped()
         initial_pose.header.frame_id = 'map'
         initial_pose.header.stamp = self.navigator.get_clock().now().to_msg()
-        initial_pose.pose = self.current_pose
+        initial_pose.pose = self.get_entity_state(self.robot_name).pose
         self.navigator.setInitialPose(initial_pose)
+        self.get_logger().info("OK")
 
+    def reset_experiments(self):
         # Generate goals
-        self.relative_goals = [
-            (r*cos(phi), r*sin(phi), delta_theta)
-            for r, phi, delta_theta in product(
+        self.expirements = [*product(
                 sample_distances, sample_directions, sample_headings)]
 
-        # Begin testing
-        self.experimental_results = []
+        # Prepare for experiments
         self.goal_pose = PoseStamped()
         self.goal_pose.header.frame_id = 'map'
         self.current_experiment_index = 0
+        time_stamp = self.navigator.get_clock().now().to_msg().sec
+        with open(data_file_path, 'w') as file:
+            file.write(str(time_stamp) + '\n')
+
+    def loop(self):
+        if self.navigator.isTaskComplete():
+            if self.current_experiment_index > 0:
+                self.collect_result()
+            input(">>Start next experiment<<")
+            self.begin_experiment(self.current_experiment_index)
+            self.current_experiment_index += 1
 
     def begin_experiment(self, experiment_index):
-        delta_x, delta_y, delta_theta = self.relative_goals[experiment_index]
-        pose = self.current_pose
+        self.get_logger().info(f"Chuẩn bị cho thử nghiệm #{experiment_index}")
+        r, phi, delta_theta = self.expirements[experiment_index]
+        pose = self.get_entity_state(self.robot_name).pose
+        yaw = yaw_from_pose(pose)
+
+        # x, y offsets
+        delta_x, delta_y = r*cos(yaw + phi), r*sin(yaw + phi)
+
+        # Set goal pose
         self.goal_pose.header.stamp = self.navigator.get_clock().now().to_msg()
         self.goal_pose.pose.position.x = pose.position.x + delta_x
         self.goal_pose.pose.position.y = pose.position.y + delta_y
-        self.goal_pose.pose.orientation = quaternion_from_yaw(
-            yaw_from_pose(pose) + delta_theta)
+        self.goal_pose.pose.orientation = quaternion_from_yaw(yaw + delta_theta)
         accepted = self.navigator.goToPose(self.goal_pose)
         goal_str = f"({delta_x:.2f}, {delta_y:.2f}, " \
                    f" {round(degrees(delta_theta))}°)"
@@ -116,30 +111,84 @@ class BeetleEvaluationManager(Node):
 
         self.get_logger().info(info + goal_str)
 
-        # TODO: add SpawnEntity or SetEntityState client
+        # Set marker pose
+        self.set_entity_state(
+            self.marker_name, self.goal_pose.pose.position.x,
+            self.goal_pose.pose.position.y, q=self.goal_pose.pose.orientation,
+        )
 
-    def collect_results(self):
+        # Set camera pose
+        self.set_entity_state(
+            self.camera_name,
+            x=(pose.position.x + self.goal_pose.pose.position.x)/2,
+            y=(pose.position.x + self.goal_pose.pose.position.x)/2,
+            z=30.0, q=self.camera_orientation,
+        )
+
+    def collect_result(self):
         result = self.navigator.getResult()
-        if result == TaskResult.SUCCEEDED:
-            self.get_logger().info("Đã đến đích!")
-            pose = self.current_pose
-            distance_error = hypot(self.goal_pose.pose.position.y - pose.position.y,
-                                   self.goal_pose.pose.position.x - pose.position.x)
-            heading_error = abs(yaw_from_pose(pose) - yaw_from_pose(self.goal_pose.pose))
 
-            self.get_logger().info(f"""
+        time_stamp = self.get_clock().now().to_msg().sec
+        pose = self.get_entity_state(self.robot_name).pose
+        distance_error = hypot(
+            self.goal_pose.pose.position.y - pose.position.y,
+            self.goal_pose.pose.position.x - pose.position.x)
+        heading_error = abs(
+            yaw_from_pose(pose) - yaw_from_pose(self.goal_pose.pose))
+
+        if result == TaskResult.SUCCEEDED:
+            success = True
+            info = f"""Đã đến đích!
                 Sai số:
                 - Khoảng cách: {distance_error} m
                 - Hướng: {round(degrees(heading_error))}°
-            """)
-            self.experimental_results.append((distance_error, heading_error))
+            """
         elif result == TaskResult.FAILED:
-            self.get_logger().info("Di chuyển đến đích thất bại...")
-            self.experimental_results.append(None)
+            success = False
+            info = "Di chuyển đến đích thất bại..."
         elif result == TaskResult.CANCELED:
             raise RuntimeError('Task bị cancel một cách bất thường')
         else:
             raise TypeError('Sai mã status trả về!')
+
+        self.get_logger().info(info)
+        with open(data_file_path, 'a') as file:
+            line = \
+                ','.join(map(str, (time_stamp, success,
+                                   distance_error, heading_error))) \
+                    + '\n'
+            file.write(line)
+
+
+
+    def get_entity_state(self, entity_name):
+        req = GetEntityState.Request()
+        if self.get_client.wait_for_service(timeout_sec=4.0):
+            req.name = entity_name
+            srv_call = self.get_client.call_async(req)
+            rclpy.spin_until_future_complete(self, srv_call)
+            result = srv_call.result()
+            if result.success:
+                return result.state
+        return None
+
+    def set_entity_state(self, entity_name, x, y, z=None, yaw=None, q=None):
+        req = SetEntityState.Request()
+        if self.set_client.wait_for_service(timeout_sec=4.0):
+            req.state.name = entity_name
+            req.state.pose.position.x = x
+            req.state.pose.position.y = y
+            if z is not None:
+                req.state.pose.position.z = z
+            if q is not None:
+                req.state.pose.orientation = q
+            elif yaw is not None:
+                req.state.pose.orientation = quaternion_from_yaw(yaw)
+            srv_call = self.set_client.call_async(req)
+            rclpy.spin_until_future_complete(self, srv_call)
+            result = srv_call.result()
+            return result.success
+        return False
 
 # Functions --------------------------------------------------------------------
 
@@ -159,7 +208,9 @@ def main(args=None):
 
     eval_manager = BeetleEvaluationManager()
 
-    rclpy.spin(eval_manager)
+    while True:
+        eval_manager.loop()
+        sleep(0.2)
 
     eval_manager.destroy_node()
     rclpy.shutdown()
